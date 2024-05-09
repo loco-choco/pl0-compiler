2ff3e84ae3fb59a11ef2add1c229e315ced8afc8
CarlosCraveiro <carlos.craveiro@usp.br>
2024-05-08 21:19:48 -0300

    Implements argument parsing

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 all:
 	gcc src/*.c -I include/ -o pl0-compiler
 
+run:
+	./pl0-compiler pl0_examples/meu_programa.pl0
+
 clean:
 	rm -rf pl0-compiler
 	rm -rf *.out
--- src/main.c
+++ src/main.c
@@ -1,7 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(void){
-  printf("Hello from pl0-compiler!\n");
-  return 0;
+#define VERSION 0.0
+#define EXPECTED_ARGUMENTS 1
+
+int main(int argc, char* argv[]) {
+    char pl0_file_name[60];
+    int number_argument = 0;
+    int incorrect_usage_of_args = 0;
+    
+    for(int i = 1; i < argc; i++) {
+        if(argv[i][0] == '-') { // this is a flag
+	        if(strcmp(argv[i],"-h") == 0) incorrect_usage_of_args = 1; //force help text
+	        else if(strcmp(argv[i],"-v") == 0) {
+                printf("Version: %.1f\n", VERSION);
+                return 0;
+            }
+            else printf("There is no option for flag %s\n",argv[i]);
+	    }
+	    else { // this is an arg
+	        //char* end;
+            if(number_argument == 0) { //first number argument
+	    	   memcpy(pl0_file_name, argv[i], 60);
+                //if(*end != '\0' || order < 2) {
+	    	    //    incorrect_usage_of_args = 1;
+	    	    //    printf("Order is not a number bigger then 2!\n");
+	    	    //}
+	        
+            } else {
+                incorrect_usage_of_args = 1;
+	    	    printf("Too many arguments!\n");
+	        }
+	        number_argument++;
+	    }
+    }
+    if(number_argument < EXPECTED_ARGUMENTS) {
+        printf("Arguments missing!\n");
+	incorrect_usage_of_args = 1;
+    }
+    
+    if(incorrect_usage_of_args == 1) {
+        printf("Usage:\n");
+        printf("$ ./pl0-compiler <file_name>\n");
+        printf("\tfile_name - source's code file name\n"); 
+        printf("Flags:\n");
+        printf("\t-h - displays help message\n");
+        printf("\t-v - displays version\n");
+        exit(-1);
+    }
+  
+    printf("Hello from pl0-compiler!\n");
+    printf("File name: %s\n", pl0_file_name);
+    return 0;
 }
